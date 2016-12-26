50df6cbfbb0f44c5532e3401f00ac40c68030d25
vss414 <vss920414@gmail.com>
2016-12-26 12:57:41 +0700

    add files yii in project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$db = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ migrations/m161226_050615_create_tables.php
@@ -0,0 +1,105 @@
+<?php
+
+use yii\db\Migration;
+
+class m161226_050615_create_tables extends Migration
+{
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => 'pk',
+            'email' => 'VARCHAR(255) NOT NULL',
+            'password' => 'VARCHAR(64) NOT NULL',
+            'token' => 'VARCHAR(64) NOT NULL',
+            'created' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+        ]);
+
+        $this->createTable('merchant', [
+            'id' => 'pk',
+            'name' => 'VARCHAR(255) NOT NULL',
+            'description' => 'TEXT'
+        ]);
+
+        $this->createTable('coupon', [
+            'id' => 'pk',
+            'title' => 'VARCHAR(255) NOT NULL',
+            'code' => 'INT'
+        ]);
+
+        $this->createTable('user_coupon', [
+            'id' => 'pk',
+            'user_id' => 'INT NOT NULL',
+            'coupon_id' => 'INT NOT NULL'
+        ]);
+
+        $this->createTable('merchant_coupon', [
+            'id' => 'pk',
+            'merchant_id' => 'INT NOT NULL',
+            'coupon_id' => 'INT NOT NULL'
+        ]);
+
+
+
+        $this->addForeignKey(
+            'fk-user_coupon-user_id',
+            'user_coupon',
+            'user_id',
+            'user',
+            'id',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-user_coupon-coupon_id',
+            'user_coupon',
+            'coupon_id',
+            'coupon',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-merchant_coupon-merchant_id',
+            'merchant_coupon',
+            'merchant_id',
+            'merchant',
+            'id',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-merchant_coupon-coupon_id',
+            'merchant_coupon',
+            'coupon_id',
+            'coupon',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            'idx-user-token',
+            'user',
+            'token',
+            true
+        );
+        $this->createIndex(
+            'idx-user_coupon',
+            'user_coupon',
+            ['user_id', 'coupon_id'],
+            true
+        );
+        $this->createIndex(
+            'idx-merchant_coupon',
+            'merchant_coupon',
+            ['merchant_id', 'coupon_id'],
+            true
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('user_coupon');
+        $this->dropTable('merchant_coupon');
+        $this->dropTable('user');
+        $this->dropTable('merchant');
+        $this->dropTable('coupon');
+    }
+}
