6e6e17eee470df039d1633f987514207ceb2c8c6
vss414 <vss920414@gmail.com>
2016-12-28 11:49:13 +0700

    add button for delete-all merchant

--- module/admin/views/merchant/index.php
+++ module/admin/views/merchant/index.php
@@ -14,6 +14,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <p>
+        <?= Html::a(Yii::t('app', 'Delete all'), ['delete-all'], [
+            'class' => 'btn btn-danger',
+            'data-confirm' => Yii::t('app', 'Are you sure you want to delete all items?'),
+        ]) ?>
+    </p>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
